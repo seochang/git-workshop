1de50034973eda103d840add91677d43f15ae330
Seo Hyun Chang (Helen) <schangeohyun@gmail.com>
2018-03-09 17:13:32 -0800

    made change on github

--- main.cpp
+++ main.cpp
@@ -3,10 +3,7 @@ using namespace std;
 
 int main() {
     cout <<"Hello world!" <<endl;
-    //this is a comment
-    cout << "*****" <<endl;
-    //this is a second comment
-    //____
+    cout <<"This change was done in github" << endl;
     return 0;
 
 }
